641d33c6fa600928536ebd00bd269ac7fd9eedba
Akshat Soni <soniakshat.official.1210@gmail.com>
2026-03-30 00:41:41 +0530

    Added LC 9_PalindromeNumbers.cpp

---
+++ Basics/9_PalindromeNumbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if( x < 0) {return false;}
+        
+        vector<int> arr;
+        
+        while(x > 0){
+            arr.push_back(x%10);
+            x = x/10;
+
+        }
+       int i=0 , j = arr[arr.size()-1];
+       while(i<j){
+        if(arr[i] != j){return false;}
+        i++;
+        j--;
+       }
+       
+return true;
+         
+    }
+};
